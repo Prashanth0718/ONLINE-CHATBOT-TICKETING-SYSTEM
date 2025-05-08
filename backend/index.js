17333ba051912ff99836ba48445f2cd9df5b1050
PRASHANTH S N <prashanthsn2003@gmail.com>
2025-05-08 13:35:33 +0530

    index file updated

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "https://museumgo-backend.onrender.com", "https://prashanthsn-museumgo.netlify.app", "https://museumgo.in/"], // ✅ Added 5174
+  origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "https://museumgo-backend.onrender.com", "https://prashanthsn-museumgo.netlify.app"], // ✅ Added 5174
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow cookies/auth headers
